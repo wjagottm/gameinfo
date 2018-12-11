7d1880accfa98d00565b98716fe7b0e9fc77d479
wjagottm <wjagottm@avans.nl>
2018-12-11 17:23:38 +0100

    fixed controller

--- controllers/developer_controller.js
+++ controllers/developer_controller.js
@@ -53,15 +53,15 @@ module.exports = {
     delete(req, res, next) {
         const developerId = req.params.id;
 
-        Game.find({ developer: developerId})
-        .then((games) => {
-            games.forEach(function(gameObj) {
-                Character.deleteMany({ game: gameObj._id})
-                    .then(() => Game.deleteMany({ developer: developerId}))
-                    .then(() => Developer.deleteOne({ _id: developerId}))
-                    .then(() => res.status(200).send())
-                    .catch(next);
-            })
-        })
+        Developer.deleteOne({ _id: developerId })
+            .then(() => Game.find({ developer: developerId })
+                .then((games) => {
+                    games.forEach(function (gameObj) {
+                        Character.deleteMany({ game: gameObj._id })
+                            .then(() => Game.deleteMany({ developer: developerId }))
+                            .then(() => res.status(200).send())
+                            .catch(next);
+                    })
+                }))
     }
 }
