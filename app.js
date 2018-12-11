121b57cf0f627d5426efaf124b0f6e7d28a1489f
wjagottm <wjagottm@avans.nl>
2018-12-11 17:19:11 +0100

    trying to fix cors

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ var jwt = require('jsonwebtoken');
 
 const app = express();
 
-app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Access-Token, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 app.set('secretKey', 'nodeRestApi'); // jwt secret token
 
